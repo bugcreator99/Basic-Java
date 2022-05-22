63cf94f1709cef634c4f996baa6beaaf8ce9671d
Forhad Hossain <104172772+bugcreator99@users.noreply.github.com>
2022-05-22 23:17:58 +0600

    Add files via upload

---
+++ forEachLoop/Test.java
@@ -0,0 +1,10 @@
+
+package forEachLoop;
+
+public class Test {
+    public static void main(String[] args) {
+        int ar[]={10,20,30,40,50,60};
+        for(int element: ar)
+            System.out.print(element+" ");
+    }
+}
---
+++ forEachLoop/Test2.java
@@ -0,0 +1,10 @@
+
+package forEachLoop;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int ar[] = {20,30,40,50,60,70};
+        for(int i=0; i<ar.length; i++)
+            System.out.println(ar[i]+" ");
+    }
+}
