63e6b466a5d1da39fe859f927906ea467eae6a6c
Forhad Hossain <104172772+bugcreator99@users.noreply.github.com>
2022-06-09 13:11:10 +0600

    Add files via upload

---
+++ jumpStatement/Continue4.java
@@ -0,0 +1,13 @@
+package jumpStatement;
+
+public class Continue4 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++)
+        {
+        if(i!=3)
+            continue;
+        else
+            System.out.print(i+" ");
+        }
+    }
+}
